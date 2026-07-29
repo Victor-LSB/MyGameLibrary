b64c973da2a353f1816cbdaa4568f055c0448a26
Victor-LSB <victor.8298203@edu.univali.br>
2026-07-29 20:55:15 -0300

    google analytics

--- src/Views/header.php
+++ src/Views/header.php
@@ -6,5 +6,14 @@
     <title>MyGameLibrary</title>
     <link rel="stylesheet" href="./assets/css/output.css">
     <link rel="icon" type="image/png" href="assets/icon/icong.png?v=1">
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-30Z4M9S4PC"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-30Z4M9S4PC');
+    </script>
 </head>
  
